6cbd9103843fea1f082a64e53e053a0c84b8cd90
Guillaume Luchet <guillaume@geelweb.org>
2023-06-19 22:01:27 +0200

    Django 4.2 compat

--- src/geelweb/django/navhelper/templatetags/navactive.py
+++ src/geelweb/django/navhelper/templatetags/navactive.py
@@ -1,5 +1,5 @@
 from django.template import Library
-from django.core.urlresolvers import resolve
+from django.urls import resolve
 from django.conf import settings
 import re
 
--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import patterns, url
+from django.urls import re_path
+from . import views
 
-urlpatterns = patterns('tests.views',
-    url(r'^main-page/$', 'page', name='p1'),
-    url(r'^main-page/sub-section/$', 'page', name='p1-s1'),
-    url(r'^second-page/$', 'page', name='p1'),
-    )
+urlpatterns = [
+    re_path(r'^main-page/$', views.page, name='p1'),
+    re_path(r'^main-page/sub-section/$', views.page, name='p1-s1'),
+    re_path(r'^second-page/$', views.page, name='p1'),
+]
